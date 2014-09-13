7589de8a35d99bd9728f20f935861455bf4a6d85
carpedm20@gmail.com <carpedm20@gmail.com>
2014-09-13 16:34:29 +0900

    trash

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -23,6 +23,8 @@ cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+rm -rf ~/dotfiles_old/.vim
+
 for file in $files; do
     #echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
